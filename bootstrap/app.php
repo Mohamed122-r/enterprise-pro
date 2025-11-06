deeec23e3dc70dd5193d712cc60fb82d061de494
Mohamed122-r <anskajamal@gmail.com>
2025-11-06 22:06:42 +0200

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,20 +6,19 @@ $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
-// استخدام الـ Kernels الأساسية من Laravel
 $app->singleton(
     Illuminate\Contracts\Http\Kernel::class,
-    Illuminate\Foundation\Http\Kernel::class
+    App\Http\Kernel::class
 );
 
 $app->singleton(
     Illuminate\Contracts\Console\Kernel::class,
-    Illuminate\Foundation\Console\Kernel::class
+    App\Console\Kernel::class
 );
 
 $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
-    Illuminate\Foundation\Exceptions\Handler::class
+    App\Exceptions\Handler::class
 );
 
 return $app;
