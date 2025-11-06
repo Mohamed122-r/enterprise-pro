d5577e717d0ea8f65814733ede39f49aa519f544
Mohamed122-r <anskajamal@gmail.com>
2025-11-06 17:52:53 +0200

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,7 @@ import './bootstrap';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import router from './router';
-import App from './App.vue';
+import App from './components/Layout/AppLayout.vue';
 
 const app = createApp(App);
 const pinia = createPinia();
